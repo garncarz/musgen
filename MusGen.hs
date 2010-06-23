f0ef98d9709cd2da61925e8d641d5f5f07b19ae8
Ondřej Garncarz <ondrej@garncarz.cz>
2010-06-23 21:39:55 +0000

    Přidání historie akordů do penalt.

--- MusGen.hs
+++ MusGen.hs
@@ -16,28 +16,28 @@ main = do {
 	newStdGen;
 	gen <- getStdGen;
 	let chordsSrc = rndChords gen
-	; let chords = createFlow chordsSrc
+	; let chords = createFlow chordsSrc []
 	; exportFlow chords;
 	print chords
 }
 
 
-rndChords :: RandomGen g => g -> [Chord]
+rndChords :: RandomGen g => g -> Flow
 rndChords g =
 	let
-		(tonesCount, g2) = randomR (1, 5) g
+		(tonesCount, g2) = randomR (0, 5) g
 		(dur, g3) = randomR (1, 30) g2
 		(g4, g5) = split g3
 		tones = take tonesCount $ randomRs (0, 127) g4
 	in (tones, dur) : rndChords g5
 
 
-createFlow :: [Chord] -> [Chord]
-createFlow (ch:rest)
-	| isEnd ch = ch : []
-	| penalty ch <= 0.5 = ch : createFlow rest
-	| otherwise = createFlow rest
+createFlow :: Flow -> Flow -> Flow
+createFlow (ch:rest) past
+	| penalty ch past <= 0.5 && isEnd ch past = [ch]
+	| penalty ch past <= 0.5 = ch : createFlow rest (ch:past)
+	| otherwise = createFlow rest past
 
-isEnd :: Chord -> Bool
-isEnd (tones, dur) = tones == [60]
+isEnd :: Chord -> Flow -> Bool
+isEnd (tones, dur) past = length past > 20
 
--- Penalties.hs
+++ Penalties.hs
@@ -3,23 +3,35 @@ module Penalties where
 import List
 import Types
 
-penalty :: Chord -> Float
-penalty ch = penaltiesSum ch penalties / genericLength penalties
+penalty :: Chord -> Flow -> Float
+penalty ch past = penaltiesSum ch past penalties
 
-penaltiesSum :: Chord -> [Chord -> Float] -> Float
-penaltiesSum _ [] = 0
-penaltiesSum ch (p:ps) = p ch + penaltiesSum ch ps
+penaltiesSum :: Chord -> Flow -> [Chord -> Flow -> Float] -> Float
+penaltiesSum _ _ [] = 0
+penaltiesSum ch past (p:ps) = p ch past + penaltiesSum ch past ps
 
-penalties :: [Chord -> Float]
-penalties = [penaltyDist, penaltySlow]
+penalties :: [Chord -> Flow -> Float]
+penalties = [penaltyJumps]
 
-penaltyDist :: Chord -> Float
-penaltyDist (tones, dur) = minimum [
+penaltyDist :: Chord -> Flow -> Float
+penaltyDist (tones, dur) _ = minimum [
 	abs (realToFrac (sum tones) / genericLength tones - 60) / 10,
 	1]
 
-penaltySlow :: Chord -> Float
-penaltySlow (_, dur)
+penaltySlow :: Chord -> Flow -> Float
+penaltySlow (_, dur) _
 	| dur > 5 = 1
 	| otherwise = 0
+
+
+oneJump :: [Tone] -> Tone -> Int
+oneJump pt t = fromEnum (not isNear) where
+	tUp = t + 2
+	tDown = t - 2
+	isNear = elem tUp pt || elem tDown pt
+
+penaltyJumps :: Chord -> Flow -> Float
+penaltyJumps (tones, _) ((ptones, _):_)
+	= fromIntegral (sum (map (oneJump ptones) tones)) / genericLength tones
+penaltyJumps _ [] = 0
 

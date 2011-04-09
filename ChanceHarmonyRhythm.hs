1f6c9d1f97b935ce733620e54d70032111316e0f
Ondřej Garncarz <ondrej@garncarz.cz>
2011-04-09 13:36:20 +0000

    ChanceHarmonyRhythm.

--- ChanceHarmonyRhythm.hs
+++ ChanceHarmonyRhythm.hs
@@ -17,7 +17,7 @@ chanceMeasureTime now _ = if remain1 - dur1 >= 0 then 1 else floatZero
 	where dur1 = dur now; remain1 = remain now
 
 chanceBeatTime now flow
-	| remain1 - dur1 `mod` beat == 0 = 1
+	| (remain1 - dur1) `mod` beat == 0 = 1
 	| otherwise = chanceCopyRhythm now flow
 	where dur1 = dur now; remain1 = remain now;
 		beat = measure now `div` beats now
